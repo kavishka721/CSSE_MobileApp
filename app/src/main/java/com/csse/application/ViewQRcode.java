e5cb1aff800729e037e3f19d7e50c88dfd56f1e5
kavishka721 <IT19156248@my.sliit.lk>
2021-10-05 10:17:41 +0530

    implement menu & view qr code

---
+++ app/src/main/java/com/csse/application/ViewQRcode.java
@@ -0,0 +1,15 @@
+package com.csse.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewQRcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_qrcode);
+    }
+
+}
